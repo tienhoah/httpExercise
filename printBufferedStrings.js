b1dbcfa1d887f1f8aca000a73bad960689fe8552
tienhoah <nickhoang11@gmail.com>
2018-08-06 19:01:20 +0000

    print output buffered strings when data is finished receiving.

--- printBufferedStrings.js
+++ printBufferedStrings.js
@@ -1,12 +1,13 @@
 var https = require('https');
+var log = console.log;
 
-function getAndPrintHTMLChunks () {
-  // var https = require('https');
+function getAndPrintHTML () {
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
+  var output = "";
 
   /* Add your code here */
   https.get(requestOptions, function (response) {
@@ -14,8 +15,14 @@ function getAndPrintHTMLChunks () {
 
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-    console.log('Chunk Received', data);
+      output += data;
+    });
+
+    response.on('end', function() {
+      log('Response stream complete.');
+      log(output);
     });
   });
 }
-getAndPrintHTMLChunks();
+
+getAndPrintHTML();
